fdd4acb1b8091b68d17595f26d1560dfd41f187f
Erick Castañeda Martínez <erickce40@gmail.com>
2021-05-10 03:31:07 -0500

    webpack funcionando más o menos bien

---
+++ practica-03/frontend-webpack/src/App.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./Home/Pages/Home";
+import SeeQuestion from "./Home/Pages/SeeQuestion";
+import QuestionForm from "./Home/Pages/QuestionForm";
+function App() {
+  // 2. Use at the root of your app
+  return (
+    <ChakraProvider>
+      <Box width="70vw" margin="0 auto" paddingTop="10">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route exact path="/see">
+              <SeeQuestion />
+            </Route>
+
+            <Route exact path="/create">
+              <QuestionForm />
+            </Route>
+
+            <Route exact path="/modify">
+              <QuestionForm />
+            </Route>
+
+            <Redirect to="/" />
+          </Switch>
+        </Router>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
